a3d7ad5d5f039c6cc8d1f0efae55f976918007b6
Gyliarde Araujo <gyliardearaujo@Air-de-Gyliarde.meuintelbras.local>
2020-08-07 22:53:26 -0300

    Configured database sqlite

---
+++ src/database/migrations/03_create_connections.ts
@@ -0,0 +1,18 @@
+import Knex from 'knex'
+
+export async function up(Knex: Knex) {
+    return Knex.schema.createTable('connections',table => {
+        table.increments('id').primary();
+
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users');
+
+        table.timestamp('created_at').defaultTo('now()').notNullable();    
+    });
+}
+
+export async function down(Knex: Knex)  {
+    return Knex.schema.dropTable('connections');
+}
